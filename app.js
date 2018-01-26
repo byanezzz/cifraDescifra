e6fd16e3f38f912ec297c591c09249bbe5786f0f
byanezzz <biancayanez89@gmail.com>
2018-01-26 00:06:34 -0300

    formato google

--- app.js
+++ app.js
@@ -1,81 +1,77 @@
-function cipher(){
-do{
-	//Valida que la frase no contenga espacios en blancos ni numeros
-	var frase=prompt("Ingrese una frase:")
-  	var invalido=false;
-	if(frase.length===0){
-		invalido=true;
-		alert("Ha olvidado ingresar la frase");
-	}else{
-	for (i=0; i<frase.length; i++){ 
-			if (frase[i]===" "||!isNaN(frase[i])){
-				invalido=true;
-				alert("La frase ingresada no es válida");
-				break;
-			}
-		}
-	}
-}while(invalido==true)
-//Cifra la frase, la convierte en codigo ASCII y almacena en array
- var arrASCII=[];
- for(i=0;i<frase.length;i++){
- 	arrASCII[i]=((frase.charCodeAt(i)-65+33)%58)+65;
- }
-var arrCifrado=[];
- for(i=0;i<arrASCII.length;i++){
- 	arrCifrado[i]=String.fromCharCode(arrASCII[i]);
- } 
- alert(arrCifrado.join(""));
- }
- 
-
-
-function decipher(){
-do{
-	//Valida que la frase no contenga espacios en blancos ni numeros
-	var frase=prompt("Frase cifrada:")
-  	var invalido=false;
-	if(frase.length===0){
-		invalido=true;
-		alert("Ha olvidado ingresar la frase");
-	}else{
-	for (i=0; i<frase.length; i++){ 
-			if (frase[i]===" "||!isNaN(frase[i])){
-				invalido=true;
-				alert("La frase ingresada no es válida");
-				break;
-			}
-		}
-	}
-}while(invalido==true)
-
-//Descifra la frase, la convierte en codigo ASCII y almacena en array
-var arrASCII=[];
-for(i=0;i<frase.length;i++){
- 	arrASCII[i]=((frase.charCodeAt(i)-65+25)%58)+65;
+function cipher() {
+  do {
+    // Valida que la frase no contenga espacios en blancos ni numeros
+    var phrase = prompt("Ingrese una frase:")
+    var invalid = false;
+    if (phrase.length === 0) {
+      invalid = true;
+      alert("Ha olvidado ingresar la frase");
+    } else {
+      for (var index = 0; index < phrase.length; index++) {
+        if (phrase[index] === " " || !isNaN(phrase[index])) {
+          invalid = true;
+          alert("La frase ingresada no es válida");
+          break;
+        }
+      }
+    }
+  } while (invalid === true)
+  // Cifra la frase, la convierte en codigo ASCII y almacena en array
+  var arrASCII = [];
+  for (var index = 0; index < phrase.length; index++) {
+    arrASCII[index] = ((phrase.charCodeAt(index) - 65 + 33) % 58) + 65;
+  }
+  var arrCipher = [];
+  for (var index = 0; index < arrASCII.length; index++) {
+    arrCipher[index] = String.fromCharCode(arrASCII[index]);
+  }
+  alert(arrCipher.join(""));
 }
 
-//
-var arrDescifrado=[];
-for(i=0;i<arrASCII.length;i++){
- 	arrDescifrado[i]=String.fromCharCode(arrASCII[i]);
-}
-alert(arrDescifrado.join(""));
+function decipher() {
+  do {
+    // Valida que la frase no contenga espacios en blancos ni numeros
+    var phrase = prompt("Frase cifrada:")
+    var invalid = false;
+    if (phrase.length === 0) {
+      invalid = true;
+      alert("Ha olvidado ingresar la frase");
+    } else {
+      for (var index = 0; index < phrase.length; index++) {
+        if (phrase[index] === " " || !isNaN(phrase[index])) {
+          invalid = true;
+          alert("La frase ingresada no es válida");
+          break;
+        }
+      }
+    }
+  } while (invalid === true)
+
+  // Descifra la frase, la convierte en codigo ASCII y almacena en array
+  var arrASCII = [];
+  for (var index = 0; index < phrase.length; index++) {
+    arrASCII[index] = ((phrase.charCodeAt(index) - 65 + 25) % 58) + 65;
+  }
+
+  var arrDecipher = [];
+  for (var index = 0; index < arrASCII.length; index++) {
+    arrDecipher[index] = String.fromCharCode(arrASCII[index]);
+  }
+  alert(arrDecipher.join(""));
 }
 
-function menu(){
-	do{
-		var respuesta = prompt("Indique el número de lo que desea hacer: 1)Cifrar - 2)Descifrar");
-		if(respuesta != ""){
-			if(respuesta == "1") {
-				cipher();
-			} else if (respuesta == "2") {
-				decipher();
-			} else {
-				alert("Ingrese una opción válida");
-			}
-		}
-	} while (respuesta === "" || (respuesta !== "1" && respuesta !== "2"));
+function menu() {
+  do {
+    var answ = prompt("Indique el número de lo que desea hacer: 1)Cifrar - 2)Descifrar");
+    if (answ !== "") {
+      if (answ === "1") {
+        cipher();
+      } else if (answ === "2") {
+        decipher();
+      } else {
+        alert("Ingrese una opción válida");
+      }
+    }
+  } while (answ === "" || (answ !== "1" && answ !== "2"));
 }
- menu();
-
+menu();
